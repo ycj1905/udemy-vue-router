53968c9449a700e4518166930e5f6a4c68035489
Matt Jhou <ycj1665@gmail.com>
2018-04-13 18:02:26 +0800

    <button class="btn btn-primary" @click="confirmed = !confirmed">Confirm</button>

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,8 +4,30 @@
         <p>Locale: {{ $route.query.locale }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
         <hr>
-        <button class="btn btn-primary">Confirm</button>
+        <button class="btn btn-primary" @click="confirmed = !confirmed">Confirm</button>
         <div style="height: 700px"></div>
         <p id="data"> SOme extra Data</p>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                confirmed: false
+            }
+        },
+        beforeRouteLeave: (to, from, next) => {
+            console.log('Confirmed ? ', this.confirmed);
+            if (this.confirmed) {
+                next();
+            } else {
+                if (confirm('Are you sure?')){
+                    next();
+                } else {
+                    next(false);
+                }
+            }
+        }
+    }
+</script>
